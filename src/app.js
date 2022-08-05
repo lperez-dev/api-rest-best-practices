92a1bd60451033cf7d9507efbe9c86361075fc3e
Luis C Perri <prluisca@gmail.com>
2022-08-05 00:02:34 -0500

    Adding a Procfile

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ const app = express()
 app.use(express.json())
 express.urlencoded({extended: false})
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
 app.get('/', (req, res) => {
     res.send('✌ Hello world')
 })
