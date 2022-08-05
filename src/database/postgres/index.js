7352b0f776504487c094bce8c9f40e44714e7b6e
Luis C Perri <prluisca@gmail.com>
2022-08-04 22:36:22 -0500

    final

--- src/database/postgres/index.js
+++ src/database/postgres/index.js
@@ -1,16 +1,38 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DATABASE } = process.env
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env
 
-const sequelize = new Sequelize(
-    DATABASE,
-    DB_USER,
-    DB_PASSWORD,
-    {
-        host: DB_HOST,
-        dialect: 'postgres'
-    }
-)
+const sequelize = process.env.NODE_ENV === 'production' 
+    ?   new Sequelize({
+            database: DB_NAME,
+            dialect: 'postgres',
+            host: DB_HOST,
+            port: 5432,
+            username: DB_USER,
+            password: DB_PASSWORD,
+            pool: {
+                max: 3,
+                min: 1,
+                idle: 10000
+            },
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: true
+                },
+                keepAlive: true
+            },
+            ssl: true
+        })
+    :   new Sequelize(
+            DB_NAME,
+            DB_USER,
+            DB_PASSWORD,
+            {
+                host: DB_HOST,
+                dialect: 'postgres'
+            }
+        )
 
 module.exports = sequelize
